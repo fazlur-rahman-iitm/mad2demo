adec51bdd13ee48b6b7b520e8962fa74c4b135c2
Md Fazlur Rahman <134944689+fazlur-rahman-iitm@users.noreply.github.com>
2024-06-23 08:58:04 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+import VueToast from 'vue-toastification';
+import 'vue-toastification/dist/theme-default.css';
 
 // vue app creation
 const app = createApp(App)
+
 
 // global configuration for axios
 app.config.globalProperties.$axios = axios
@@ -15,5 +18,8 @@ axios.defaults.baseURL='http://127.0.0.1:5000'
 
 //adding router to the app
 app.use(router)
+
+// adding toast
+app.use(VueToast)
 
 app.mount('#app')
