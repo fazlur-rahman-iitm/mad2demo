1d9ab4fbdd9852a730c6a4678df3b92e274c03e8
Md Fazlur Rahman <134944689+fazlur-rahman-iitm@users.noreply.github.com>
2024-06-23 09:10:05 +0530

    added axios request interceptor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,6 +16,15 @@ app.config.globalProperties.$axios = axios
 //default base url configuration for axios
 axios.defaults.baseURL='http://127.0.0.1:5000'
 
+// setting up interceptor to send to backend api everytime with each axios request
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('acess_token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 //adding router to the app
 app.use(router)
 
