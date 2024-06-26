a8ecb26a567899a1277d0e7bf7e0eebf4d75b5fd
Md Fazlur Rahman <134944689+fazlur-rahman-iitm@users.noreply.github.com>
2024-06-26 12:35:57 +0530

    Delete frontend/src directory

--- frontend/src/views/AdminDashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>this is Admin dashboard</h1>
-      
-    </div>
-  </template>
--- frontend/src/views/UserDashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>this is user dashboard</h1>
-      
-    </div>
-  </template>
